02384ba8dd4b0d9810b757c95c996fcf917430b4
Wmoreno211057 <39283559+Wmoreno211057@users.noreply.github.com>
2022-07-13 15:51:49 -0400

    [ADD] app payroll_policy_rounding

---
+++ payroll/tests/common.py
@@ -0,0 +1,200 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from datetime import datetime, timedelta
+
+from odoo.fields import Date
+from odoo.tests.common import TransactionCase
+
+
+class TestPayslipBase(TransactionCase):
+    def setUp(self):
+        super(TestPayslipBase, self).setUp()
+
+        self.Payslip = self.env["hr.payslip"]
+        self.RuleInput = self.env["hr.rule.input"]
+        self.SalaryRule = self.env["hr.salary.rule"]
+        self.SalaryRuleCateg = self.env["hr.salary.rule.category"]
+
+        # Salary Rule Categories
+        #
+        self.categ_basic = self.SalaryRuleCateg.create(
+            {
+                "name": "Basic",
+                "code": "BASIC",
+            }
+        )
+        self.categ_alw = self.SalaryRuleCateg.create(
+            {
+                "name": "Allowance",
+                "code": "ALW",
+            }
+        )
+        self.categ_gross = self.SalaryRuleCateg.create(
+            {
+                "name": "Gross",
+                "code": "GROSS",
+            }
+        )
+        self.categ_ded = self.SalaryRuleCateg.create(
+            {
+                "name": "Deduction",
+                "code": "DED",
+            }
+        )
+        self.categ_net = self.SalaryRuleCateg.create(
+            {
+                "name": "NET",
+                "code": "NET",
+            }
+        )
+
+        #
+        # Salary Rules
+        #
+
+        # Earnings
+        #
+        self.rule_basic = self.SalaryRule.create(
+            {
+                "name": "Basic Salary",
+                "code": "BASIC",
+                "sequence": 1,
+                "category_id": self.categ_basic.id,
+                "condition_select": "none",
+                "amount_select": "code",
+                "amount_python_compute": "result = contract.wage",
+            }
+        )
+        self.rule_hra = self.SalaryRule.create(
+            {
+                "name": "House Rent Allowance",
+                "code": "HRA",
+                "sequence": 5,
+                "category_id": self.categ_alw.id,
+                "condition_select": "none",
+                "amount_select": "percentage",
+                "amount_percentage": 40.0,
+                "amount_percentage_base": "contract.wage",
+            }
+        )
+        self.rule_meal = self.SalaryRule.create(
+            {
+                "name": "Meal Voucher",
+                "code": "MA",
+                "sequence": 16,
+                "category_id": self.categ_alw.id,
+                "condition_select": "none",
+                "amount_select": "fix",
+                "amount_fix": 10.0,
+                "quantity": "worked_days.WORK100 and worked_days.WORK100.number_of_days",
+            }
+        )
+        self.rule_commission = self.SalaryRule.create(
+            {
+                "name": "Get 1% of sales",
+                "code": "SALE",
+                "sequence": 17,
+                "category_id": self.categ_alw.id,
+                "condition_select": "none",
+                "amount_select": "code",
+                "amount_python_compute": "result = "
+                "(inputs.SALEURO and inputs.SALEURO.amount) * 0.01",
+            }
+        )
+        self.RuleInput.create(
+            {
+                "name": "Sales to Europe",
+                "code": "SALEURO",
+                "input_id": self.rule_commission.id,
+            }
+        )
+
+        # Gross
+        #
+        self.rule_gross = self.SalaryRule.create(
+            {
+                "name": "Gross",
+                "code": "GROSS",
+                "sequence": 100,
+                "category_id": self.categ_gross.id,
+                "condition_select": "none",
+                "amount_select": "code",
+                "amount_python_compute": "result = categories.BASIC + categories.ALW",
+            }
+        )
+
+        # Deductions
+        #
+        self.rule_proftax = self.SalaryRule.create(
+            {
+                "name": "Professional Tax",
+                "code": "PT",
+                "sequence": 150,
+                "category_id": self.categ_ded.id,
+                "condition_select": "none",
+                "amount_select": "fix",
+                "amount_fix": -200.0,
+            }
+        )
+
+        # Net
+        #
+        self.rule_net = self.SalaryRule.create(
+            {
+                "name": "Net",
+                "code": "NET",
+                "sequence": 200,
+                "category_id": self.categ_net.id,
+                "condition_select": "none",
+                "amount_select": "code",
+                "amount_python_compute": "result = categories.BASIC "
+                "+ categories.ALW + categories.DED",
+            }
+        )
+
+        # I create a new employee "Richard"
+        self.richard_emp = self.env["hr.employee"].create(
+            {
+                "name": "Richard",
+                "gender": "male",
+                "birthday": "1984-05-01",
+                "country_id": self.ref("base.be"),
+                "department_id": self.ref("hr.dep_rd"),
+            }
+        )
+
+        # I create a salary structure for "Software Developer"
+        self.developer_pay_structure = self.env["hr.payroll.structure"].create(
+            {
+                "name": "Salary Structure for Software Developer",
+                "code": "SD",
+                "company_id": self.ref("base.main_company"),
+                "rule_ids": [
+                    (4, self.rule_hra.id),
+                    (4, self.rule_proftax.id),
+                    (4, self.rule_meal.id),
+                    (4, self.rule_commission.id),
+                    (4, self.rule_basic.id),
+                    (4, self.rule_gross.id),
+                    (4, self.rule_net.id),
+                ],
+            }
+        )
+
+        # I create a contract for "Richard"
+        self.env["hr.contract"].create(
+            {
+                "date_end": Date.to_string(datetime.now() + timedelta(days=365)),
+                "date_start": Date.today(),
+                "name": "Contract for Richard",
+                "wage": 5000.0,
+                "employee_id": self.richard_emp.id,
+                "struct_id": self.developer_pay_structure.id,
+                "kanban_state": "done",
+            }
+        )
+
+    def apply_contract_cron(self):
+        self.env.ref(
+            "hr_contract.ir_cron_data_contract_update_state"
+        ).method_direct_trigger()
---
+++ payroll/tests/test_payslip_flow.py
@@ -0,0 +1,153 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo.tests import Form
+from odoo.tools import test_reports
+
+from .common import TestPayslipBase
+
+
+class TestPayslipFlow(TestPayslipBase):
+    def setUp(self):
+        super().setUp()
+
+        self.test_rule = self.SalaryRule.create(
+            {
+                "name": "Test rule",
+                "code": "TEST",
+                "category_id": self.categ_alw.id,
+                "sequence": 5,
+                "amount_select": "code",
+                "amount_python_compute": "result = contract.wage",
+            }
+        )
+
+    def test_00_payslip_flow(self):
+        """Testing payslip flow and report printing"""
+
+        # I put all eligible contracts (including Richard's) in an "open" state
+        self.apply_contract_cron()
+
+        # I create an employee Payslip
+        frm = Form(self.Payslip)
+        frm.employee_id = self.richard_emp
+        richard_payslip = frm.save()
+
+        payslip_input = self.env["hr.payslip.input"].search(
+            [("payslip_id", "=", richard_payslip.id), ("code", "=", "SALEURO")]
+        )
+        # I assign the amount to Input data
+        payslip_input.write({"amount": 5.0})
+
+        # I verify the payslip is in draft state
+        self.assertEqual(richard_payslip.state, "draft", "State not changed!")
+
+        context = {
+            "lang": "en_US",
+            "tz": False,
+            "active_model": "ir.ui.menu",
+            "department_id": False,
+            "section_id": False,
+            "active_ids": [self.ref("payroll.hr_payslip_menu")],
+            "active_id": self.ref("payroll.hr_payslip_menu"),
+        }
+        # I click on 'Compute Sheet' button on payslip
+        richard_payslip.with_context(context).compute_sheet()
+
+        # Find the 'NET' payslip line and check that it adds up
+        # salary + HRA + MA + SALE - PT
+        work100 = richard_payslip.worked_days_line_ids.filtered(
+            lambda x: x.code == "WORK100"
+        )
+        line = richard_payslip.line_ids.filtered(lambda l: l.code == "NET")
+        self.assertEqual(len(line), 1, "I found the 'NET' line")
+        self.assertEqual(
+            line[0].amount,
+            5000.0 + (0.4 * 5000) + (work100.number_of_days * 10) + 0.05 - 200.0,
+            "The 'NET' amount equals salary plus allowances - deductions",
+        )
+
+        # Then I click on the 'Confirm' button on payslip
+        richard_payslip.action_payslip_done()
+
+        # I verify that the payslip is in done state
+        self.assertEqual(richard_payslip.state, "done", "State not changed!")
+
+        # I want to check refund payslip so I click on refund button.
+        richard_payslip.refund_sheet()
+
+        # I check on new payslip Credit Note is checked or not.
+        payslip_refund = self.env["hr.payslip"].search(
+            [
+                ("name", "like", "Refund: " + richard_payslip.name),
+                ("credit_note", "=", True),
+            ]
+        )
+        self.assertTrue(bool(payslip_refund), "Payslip not refunded!")
+
+        # I want to generate a payslip from Payslip run.
+        payslip_run = self.env["hr.payslip.run"].create(
+            {
+                "date_end": "2011-09-30",
+                "date_start": "2011-09-01",
+                "name": "Payslip for Employee",
+            }
+        )
+
+        # I create record for generating the payslip for this Payslip run.
+
+        payslip_employee = self.env["hr.payslip.employees"].create(
+            {"employee_ids": [(4, self.richard_emp.id)]}
+        )
+
+        # I generate the payslip by clicking on Generat button wizard.
+        payslip_employee.with_context(active_id=payslip_run.id).compute_sheet()
+
+        # I open Contribution Register and from there I print the Payslip Lines report.
+        self.env["payslip.lines.contribution.register"].create(
+            {"date_from": "2011-09-30", "date_to": "2011-09-01"}
+        )
+
+        # I print the payslip report
+        data, data_format = self.env.ref(
+            "payroll.action_report_payslip"
+        )._render_qweb_pdf(richard_payslip.ids)
+
+        # I print the payslip details report
+        data, data_format = self.env.ref(
+            "payroll.payslip_details_report"
+        )._render_qweb_pdf(richard_payslip.ids)
+
+        # I print the contribution register report
+        context = {
+            "model": "hr.contribution.register",
+            "active_ids": [self.ref("payroll.hr_houserent_register")],
+        }
+        test_reports.try_report_action(
+            self.env.cr,
+            self.env.uid,
+            "action_payslip_lines_contribution_register",
+            context=context,
+            our_module="payroll",
+        )
+
+    def test_contract_qty(self):
+
+        # I set the test rule to detect contract count
+        self.test_rule.amount_python_compute = (
+            "result = payroll.contracts and payroll.contracts.count or -1.0"
+        )
+        self.developer_pay_structure.write({"rule_ids": [(4, self.test_rule.id)]})
+
+        # I put all eligible contracts (including Richard's) in an "open" state
+        self.apply_contract_cron()
+
+        # I create an employee Payslip and process it
+        richard_payslip = self.Payslip.create({"employee_id": self.richard_emp.id})
+        richard_payslip.onchange_employee()
+        richard_payslip.compute_sheet()
+
+        line = richard_payslip.line_ids.filtered(lambda l: l.code == "TEST")
+        self.assertEqual(len(line), 1, "I found the Test line")
+        self.assertEqual(
+            line[0].amount, 1.0, "The calculated dictionary value 'contracts.qty' is 1"
+        )
